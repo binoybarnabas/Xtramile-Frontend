0b6b3e4e4aaf51b771a471a734150ee079b06039
Abhijit Thankachan <abhijit.thankachan@experionglobal.com>
2024-02-03 14:37:38 +0530

    fix: updated api endpoints

--- src/app/services/employeeServices/requestServices/request.service.ts
+++ src/app/services/employeeServices/requestServices/request.service.ts
@@ -57,15 +57,14 @@ export class RequestService {
 
   ///Get all available options with reqId
   getDataFromAvailOptions(reqId: number): Observable<any> {
-    const apiUrl = `http://localhost:5190/api/employee/viewoptions/request/${reqId}`;
-    return this.http.get(apiUrl);
+    return this.http.get(this.apiURL+ `/employee/viewoptions/request/${reqId}`);
   }
 
   //Post a selected option with optionId, employeeId, requestId
   postSelection(requestId: number, employeeId: number, optionId: number): Observable<any> {
     const body = { requestId: requestId, empId: employeeId, optionId: optionId };
     console.log(body)
-    return this.http.post('http://localhost:5190/api/requestmapping/add/option', body);
+    return this.http.post(this.apiURL + '/employee/add/option', body);
   }
 
     getRequestsPendingStatus(empId: number): Observable<PendingRequest[]> {
