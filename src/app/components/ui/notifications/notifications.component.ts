6830e33af3097e9c451941fb3d8158596b1f5527
Bivina M V <bivina.mv@experionglobal.local>
2024-02-13 13:16:25 +0530

    notifications for employee and manager

---
+++ src/app/components/ui/notifications/notifications.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+
+@Component({
+  selector: 'app-notifications',
+  templateUrl: './notifications.component.html',
+  styleUrls: ['./notifications.component.css']
+})
+
+
+export class NotificationsComponent {
+
+  @Input()
+  requestNotification: any= []
+
+  constructor() {
+  }
+  ngAfterViewInit(){
+    console.log("after view init",this.requestNotification)
+  }
+}
--- src/app/components/ui/top-bar-user/top-bar-user.component.ts
+++ src/app/components/ui/top-bar-user/top-bar-user.component.ts
@@ -1,6 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { SideNavBarService } from 'src/app/services/employeeServices/layoutServices/side-nav-bar.service';
 import { CustomDatePipe } from 'src/app/pipes/CustomDate/custom-date.pipe';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { NotificationsComponent } from '../notifications/notifications.component';
+import { RequestService } from 'src/app/services/employeeServices/requestServices/request.service';
+import { ManagerTravelRequestsService } from 'src/app/services/managerServices/travelRequestsServices/manager-travel-requests.service';
+
 @Component({
   selector: 'app-top-bar-user',
   templateUrl: './top-bar-user.component.html',
@@ -15,9 +20,16 @@ export class TopBarUserComponent {
   @Input() employeeName:string = ''
   @Input() designation: string =''
 
+  notificationModalRef?: BsModalRef;
 
-  constructor(private sideNavBarService: SideNavBarService) {
-    this.isSideNavBarCollapsed = sideNavBarService.isSideNavBarCollapsed;
+  empId= 0;
+
+  requestNotification: any= []
+
+  constructor(private sideNavBarService: SideNavBarService,private modalService: BsModalService,
+     private requestService: RequestService,private managerService :ManagerTravelRequestsService) 
+     
+     {this.isSideNavBarCollapsed = sideNavBarService.isSideNavBarCollapsed;
     this.date = Date.now();
   }
 
@@ -31,4 +43,43 @@ export class TopBarUserComponent {
     this.logoutEvent.emit("logout"); // Emit logout event
   }
 
+  ngOnInit(){
+    if(localStorage.getItem('userData')){
+      const userData= JSON.parse(localStorage.getItem('userData')!)
+      this.empId=userData.empId;
+    }
+    this.getRequestNotification();
+  }
+
+  openNotificationModal(){
+    this.notificationModalRef= this.modalService.show(NotificationsComponent,{
+      backdrop: false,
+      initialState: {requestNotification: this.requestNotification},
+    } )
+    
+  }
+
+  closeModal(){
+    this.modalService.hide();
+  }
+
+  getRequestNotification(){
+    if (this.designation=='Employee'){
+      this.requestService.getEmployeeRequestNotification(this.empId).subscribe(
+        {
+          next: (data) =>{
+            this.requestNotification=data;
+            console.log(this.requestNotification)
+          }
+        });
+    }
+    else if(this.designation=='Manager'){
+      this.managerService.getManagerRequestNotification(this.empId).subscribe({
+        next: (data) =>{
+          this.requestNotification=data;
+        }
+      }
+      );
+    }
+  }
 }
