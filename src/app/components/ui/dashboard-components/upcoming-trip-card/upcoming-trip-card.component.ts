3c0124bb88101ab06d0bb6b6c3afdef2b119f3eb
Muhammed Suhail M K <muhammed.mk@experionglobal.local>
2024-06-18 23:48:24 +0530

    Code Cleaning

---
+++ src/app/components/ui/dashboard-components/upcoming-trip-card/upcoming-trip-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upcoming-trip-card',
+  templateUrl: './upcoming-trip-card.component.html',
+  styleUrls: ['./upcoming-trip-card.component.css']
+})
+export class UpcomingTripCardComponent {
+
+}
